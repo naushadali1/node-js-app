60811a74248c11837ad4128671df679be5366654
Naushad Ali <nashadali585@gmail.com>
2024-08-05 15:39:40 +0500

    Cfreate server

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require("http");
+const server = http.createServer((req, res) => {
+  console.log(req);
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+  console.log("URL: http://localhost:3000");
+});
---
+++ node-js-app.js
@@ -0,0 +1,10 @@
+// console.log("Starting node server ....");
+// const fs = require("fs");
+// fs.writeFileSync("server.txt","welcome to node server");
+
+// rest oprater
+const toArr = (...args) => {
+  return args;
+};
+console.log(toArr(1, 2, 3, 4, 5));
+
