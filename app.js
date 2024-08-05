a4e93fbeea23286aeb75957b06c5f3a2e5dedb67
Naushad Ali <nashadali585@gmail.com>
2024-08-05 17:05:06 +0500

    http worming onn request and response

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const http = require("http");
+const fs = require("fs");
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write(
+      "<body> <form action='/message' name='message' method='POST' > <input type='text'> </input> <br> <button type = 'submit'> send </button> </form> </body>"
+    )
+    res.write("</html>")
+    return res.end()
+  }
+  if (url === "/message" && method === "POST") {
+    res.setHeader("Location", "/");
+    res.statusCode = 302;
+    fs.writeFileSync("Message.txt", "This is message file");
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<body> <h1> welcome to the server</h1> </body>");
+  res.write("</html>");
 });
 
 server.listen(3000, () => {
